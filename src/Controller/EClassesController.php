7c16cbf04da1ae81a204f0d41bbf382db9e3496b
NedjZa8 <annienedjolbe1@gmail.com>
2022-05-17 08:43:25 +0000

    creer classe

--- src/Controller/EClassesController.php
+++ src/Controller/EClassesController.php
@@ -2,17 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\Classe;
+use App\Form\ClasseType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EClassesController extends AbstractController
 {
     #[Route('/e/classes', name: 'ecole_classes')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $classeRepo=$doctrine->getRepository(Classe::class);
+        $classes=$classeRepo->findAll();
         return $this->render('e_classes/index.html.twig', [
-            'controller_name' => 'EClassesController',
+            'mesClasses' => $classes,
+        ]);
+    }
+
+    // ********************************CREATION******************
+
+    #[Route('/e/classes/add', name: 'ecole_classes_add')]
+    public function add(Request $request, ManagerRegistry $r): Response
+    {
+        $classe=new Classe();
+        #$classe->setDataCreation(new \DateTime());
+
+        $form=$this->createForm(ClasseType::class, $classe);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $classe=$form->getData();
+
+            $em=$r->getManager();
+
+            $em->persist($classe);
+            $em->flush();
+
+            $this->addFlash("success","La classe".$classe->getNom()."a ete enregistré");
+            return $this->redirectToRoute("ecole_classes");
+        }
+        return $this->render('e_classes/add.html.twig', [
+            "formulaire"=>$form->createView()
         ]);
     }
 }
+
+
